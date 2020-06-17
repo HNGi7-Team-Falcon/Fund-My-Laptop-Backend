0ff609ff5fb4e95dbb5cea82217fdc7354e6923c
saucecodee <ashawinta@gmail.com>
2020-06-17 18:38:30 +0100

    fix: broken links

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const server = http.createServer(app);
 const preMiddlewares = require('./src/middlewares/preMiddlewares');
 const errorMiddlewares = require('./src/middlewares/errorMiddlewares');
 const routes = require('./src/routes');
-const databaseConfig = require('./src/config');
+// const databaseConfig = require('./src/config');
 const port = process.env.PORT;
 
-preMiddlewares;
+preMiddlewares(app);
 
-app.use('/api', routes())
+// app.use('/api', routes())
 
 app.use('/', (req, res) => {
-  res.status(200).sendFile(express.static("public/index.html"));
+  res.status(200).send("Fund my laptop is under construction");
 })
 
 errorMiddlewares(app)
 
 server.listen(port, () => {
   console.log(`::: server listening on port ${port}. Open via http://localhost:${port}/`);
-  databaseConfig();
+
 });
 
 server.on('error', (error) => {
