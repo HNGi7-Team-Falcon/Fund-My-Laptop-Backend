8b2bfb3a2105d917882f807a8c2d4bca9df98964
saucecodee <ashawinta@gmail.com>
2020-06-17 18:56:41 +0100

    fix: clear dead code

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
-const preMiddlewares = require('./src/middlewares/preMiddlewares');
-const errorMiddlewares = require('./src/middlewares/errorMiddlewares');
-const routes = require('./src/routes');
+// const preMiddlewares = require('./src/middlewares/preMiddlewares');
+// const errorMiddlewares = require('./src/middlewares/errorMiddlewares');
+// const routes = require('./src/routes');
 // const databaseConfig = require('./src/config');
-const port = process.env.PORT;
+const port = 2020 || process.env.PORT;
 
 // preMiddlewares(app);
 
@@ -18,7 +18,7 @@ app.use('/', (req, res) => {
   res.status(200).send("Fund my laptop is under construction");
 })
 
-errorMiddlewares(app)
+// errorMiddlewares(app)
 
 server.listen(port, () => {
   console.log(`::: server listening on port ${port}. Open via http://localhost:${port}/`);
@@ -30,4 +30,4 @@ server.on('error', (error) => {
 });
 
 
-module.exports = app
+// module.exports = app
