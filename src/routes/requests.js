69699564ef6afaa1d50f506d4c1e3ff4088f2acd
sodiq oyedotun <oyedotunsodiq045@gmail.com>
2020-06-17 22:19:47 +0100

    Update Request route

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const {
-  createRequest
+  createRequest,
+  updateRequest
 } = require('../controllers/requests');
 
 const router = express.Router();
 
 router
     .route('/')
+    .get(getRequests)
     .post(createRequest);
+
+router
+  .route('/:id')
+  .put(updateRequest)
 
 module.exports = router;
