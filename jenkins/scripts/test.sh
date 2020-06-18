41256a75a1244452c04458e26ea387726fdd9811
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-18 18:31:30 +0100

    permissions

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+
 echo 'The following "npm" command (if executed) installs the "cross-env"'
 echo 'dependency into the local "node_modules" directory, which will ultimately'
 echo 'be stored in the Jenkins home directory. As described in'
