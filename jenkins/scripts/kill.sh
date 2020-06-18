8c867d4df9ff1f9ce5f839efb280583c43cedbdd
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-18 18:38:59 +0100

    permissions

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
