69aea729a07bba9238de1c8d0f0638e735184f89
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-18 20:27:38 +0100

    setup test

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ require('dotenv').config()
 const preMiddlewares = require("./src/middlewares/preMiddlewares");
 const errorMiddlewares = require("./src/middlewares/errorMiddlewares");
 const routes = require("./src/routes");
-const databaseConfig = require("./src/config/db");
-const port = process.env.PORT;
 
 preMiddlewares(app);
 
@@ -21,13 +19,6 @@ app.use("/", (req, res) => {
 });
 
 errorMiddlewares(app);
-
-app.listen(port, () => {
-  console.log(
-    `::: server listening on port ${port}. Open via http://localhost:${port}/`
-  );
-  databaseConfig();
-});
 
 app.on("error", (error) => {
   console.log(`::> an error occiurred in our server: \n ${error}`);
---
+++ src/bin/www.js
@@ -0,0 +1,11 @@
+const app = require('../../server');
+const databaseConfig = require('../config/db');
+const port = process.env.PORT;
+
+
+app.listen(port, () => {
+  console.log(
+    `::: server listening on port ${port}. Open via http://localhost:${port}/`
+  );
+  databaseConfig();
+});
