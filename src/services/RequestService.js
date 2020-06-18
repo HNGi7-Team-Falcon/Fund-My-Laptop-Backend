847753415388ffaef2eb8831aa9c7bfe132955c8
Jezeh Priesten <jepriesten@gmail.com>
2020-06-18 04:12:28 +0100

    Delete request first attempt

--- src/services/RequestService.js
+++ src/services/RequestService.js
@@ -2,7 +2,6 @@ const Request = require("./../models/Request");
 const CustomError = require("./../utils/CustomError");
 
 class RequestService {
-
   async create(data) {
     const request = new Request(data);
     await request.save();
@@ -11,15 +10,34 @@ class RequestService {
       uid: request._id,
       name: request.name,
       email: request.email,
-    }
+    };
   }
 
   async update(data) {
-    
-
-    return data
+    return data;
   }
-  
+
+  async delete(req, res) {
+    Request.findByIdAndRemove(req.params.userId)
+      .then((request) => {
+        if (!request) {
+          return res.status(404).send({
+            message: "User not found with id " + req.params.userId,
+          });
+        }
+        res.status(200).send({ message: "User deleted successfully!" });
+      })
+      .catch((err) => {
+        if (err.kind === "ObjectId" || err.name === "NotFound") {
+          return res.status(404).send({
+            message: "User not found with id " + req.params.userId,
+          });
+        }
+        return res.status(404).send({
+          message: "Could not delete user with id " + req.params.userId,
+        });
+      });
+  }
 }
 
-module.exports = new RequestService()
+module.exports = new RequestService();
