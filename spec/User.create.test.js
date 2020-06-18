90b1206f4d97972bae4ecb0ef05b5adad68df17a
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-18 22:40:23 +0100

    fix test

--- spec/User.create.test.js
+++ spec/User.create.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const app = require('../server');
 
-describe('Update user info', () => {
-  it('should update user info', async () => {
+describe('TEST:Create user route', () => {
+  it('should create a new user', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
   });
