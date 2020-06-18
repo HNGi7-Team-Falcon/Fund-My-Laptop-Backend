c3d06d4eefd20f63a613d92c5b951cbb3f4b82c2
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-18 23:40:22 +0100

    add dbhandler

---
+++ spec/db-handler.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+
+module.exports.connect = async () => {
+
+    const dbOptions = {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    };
+    
+    await mongoose
+    .connect(uri, dbOptions)
+    .then(() => console.log("Connected to database!"))
+    .catch((error) => console.log("Error!. Couldn't connect to database ", error));
+};
+
+module.exports.closeDatabase = async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+};
+
+module.exports.clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany();
+  }
+}
