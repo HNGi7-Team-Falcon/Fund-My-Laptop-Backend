7e709cc370102c22cfa3c3a1ad56fc524557886c
tobbhie <tobifolajin@gmail.com>
2020-06-19 17:18:24 +0100

    Conflict resolved

--- src/routes/requestRoute.js
+++ src/routes/requestRoute.js
@@ -5,18 +5,10 @@ const AdminRequestCtrl = require("./../controllers/admin/RequestController");
 
 
 module.exports = () => {
-<<<<<<< HEAD
-  router.get("/:requestId", RequestCtrl.findById);
-  router.post("/", RequestCtrl.create);
-
-  router.put("/:requestId", RequestCtrl.update);
-  router.delete("/:requestId", RequestCtrl.delete);
-=======
   router.get("/:requestId", authenticate, RequestCtrl.findById);
   router.post("/", authenticate, RequestCtrl.create);
   router.put("/:requestId", authenticate, RequestCtrl.update);
   router.delete("/:requestId", authenticate, RequestCtrl.delete);
->>>>>>> 630feb01b99d796aa2bbe78ad7db29721e4a1575
 
   router.get("/", authenticate, RequestCtrl.getRequests);
 
