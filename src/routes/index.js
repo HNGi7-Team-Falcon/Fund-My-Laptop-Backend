9af31d8e89cf48e1793d674b44bd961131953f54
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-19 19:22:04 +0100

    chore

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,22 +2,13 @@ const router = require("express").Router();
 
 const userRoute = require("./userRoute");
 const requestRoute = require("./requestRoute");
-const recommendationRoute = require("./recommendationRoute");
 const paymentRoute = require("./paymentRoute");
-
-const TrendCtrl = require('../controllers/TrendController');
 
 module.exports = () => {
   router.get("/test", (req, res) => res.send("Yeah it works!"));
 
   router.use("/users", userRoute());
   router.use("/request", requestRoute());
-  router.use("/vouch", recommendationRoute());
   router.use("/payment", paymentRoute);
-  router.use("/trend", TrendCtrl.getTrends);
-
-
-
-
-     return router;
+  return router;
 };
