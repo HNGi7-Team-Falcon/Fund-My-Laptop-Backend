7198b29a7eee688e74bc91af1d6d86f24ae14562
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-19 19:54:16 +0100

    chore

--- src/routes/requestRoute.js
+++ src/routes/requestRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authenticate = require("../middlewares/authenticatorMiddleware");
 const RequestCtrl = require("./../controllers/RequestController");
-const AdminRequestCtrl = require("./../controllers/admin/RequestController");
+// const AdminRequestCtrl = require("./../controllers/admin/RequestController");
 
 module.exports = () => {
   router.get("/:requestId", authenticate, RequestCtrl.findById);
