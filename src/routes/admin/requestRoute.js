85af546b2dffff7590114bfbac989d10e4bcd9be
boluakins <boluakins@gmail.com>
2020-06-20 10:48:09 +0100

    deleted outdated route

--- src/routes/admin/requestRoute.js
+++ src/routes/admin/requestRoute.js
@@ -6,7 +6,6 @@ const AdminRequestCtrl = require("./../../controllers/admin/RequestController");
 module.exports = () => {
 
   router.get("/", authenticate, AdminRequestCtrl.getRequests);
-  //router.get("/funded", authenticate, AdminRequestCtrl.getFundedRequests);
   router.get("/completed", authenticate, AdminRequestCtrl.getCompletedRequests);
   router.get("/suspended", authenticate, AdminRequestCtrl.getSuspendedRequests);
   router.patch("/suspend", authenticate, AdminRequestCtrl.suspendRequest);
