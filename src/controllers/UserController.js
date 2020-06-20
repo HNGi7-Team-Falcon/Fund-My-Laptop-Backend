859970ede001de8c3fdfbf92a413339f5649383d
saucecodee <ashawinta@gmail.com>
2020-06-21 00:32:06 +0100

    fix: delete duplicated user ctrl file

--- src/controllers/UserController.js
+++
@@ -1,7 +0,0 @@
-const AdminServ = require("../services/AdminService");const response = require("../utils/response");
-const CustomError = require('../utils/CustomError')class UserContoller {
-  async register(req, res) {
-    const data = await AdminServ.create(req.body);
-    res.status(201).send(response("User account created", data));
-  }
-}
