9d82dc0bbac22e2ff33135ab6f2b6d7c2e8e4cb6
boluakins <boluakins@gmail.com>
2020-06-20 10:55:47 +0100

    fixed bug

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,9 +5,7 @@ const User = require("../models/User");
 class UserContoller {
   async create(req, res) {
     if (!req.body) throw new CustomError("No data provided");
-  }
 
-    async create(req, res) {
     const data = await UserServ.create(req.body);
 
     res.status(201).send(response("User account created", data));
