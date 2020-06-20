ef1608a5e4f05a39dc8fb680699d8369cd37e8cd
abdul-lahidris <abdul-lahidris@users.noreply.github.com>
2020-06-20 21:14:56 +0100

    BVN VERIFICATION SERVICE

---
+++ src/services/BvnVerification.js
@@ -0,0 +1,25 @@
+//var userBVN = '123456789';
+require('dotenv').config();
+var request = require('request');
+
+
+if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+  var secretKey = 'FLWSECK_TEST-SANDBOXDEMOKEY-X';//development
+} else {
+  var secretKey = process.env.BVN_FLUTTERWAVE_SECRET_KEY;//production
+}
+
+const makeBvnRequest = (userBVN, callback) => {
+  var options = {
+    'method': 'GET',
+    'url': `https://api.flutterwave.com/v3/kyc/bvns/${userBVN}`,
+    'headers': {
+      'Authorization': `Bearer ${secretKey}`
+    }
+  }
+  request(options, (error, response) => {
+    if (error) { return callback(error); }
+    return callback(response.body);
+  });
+}
+  module.exports.verifyBVN = makeBvnRequest;
