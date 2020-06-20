42566d1d18b9c7ee5eff053d07b70f0192f0cb3f
Chia Gabriel <chiagabrielson@gmail.com>
2020-06-20 18:40:18 +0100

    login and signup route ready

--- src/models/User.js
+++ src/models/User.js
@@ -1,66 +1,75 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please add a valid email",
-    ],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    // select: false, // Couldn't login in with this
-  },
-  number: {
-    type: Number,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  verified: {
-    type: Boolean,
-    default: false
-  },
-  investment_count: {
-    type: Number,
-    default: 0,
-  },
-  token: {
-    type: String,
-    required: true,
-  },
-  request_count: {
-    type: Number,
-    default: 0,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+    social_verification_number: {
+        type: Number,
+        required: true
+    },
+    address: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    account_type: {
+        type: String,
+        required: true
+    },
+    verified: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    investment_count: {
+        type: Number,
+        default: 0
+    },
+    request_count: {
+        type: Number,
+        default: 0
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: user
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+})
 
 // Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
-UserSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 10);
-});
-
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('user', UserSchema);
