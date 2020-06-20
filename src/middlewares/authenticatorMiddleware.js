4ef1f840fc7edf302b5fe7b7009ede4437e47803
Chia Gabriel <chiagabrielson@gmail.com>
2020-06-20 18:16:26 +0100

    login and signup route ready

--- src/middlewares/authenticatorMiddleware.js
+++ src/middlewares/authenticatorMiddleware.js
@@ -1,31 +1,46 @@
-
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const response = require("../utils/response");
 
 const jwtSecret = process.env.JWT_SECRET;
 
-const auth = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer", "").trim();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-  const decoded = jwt.verify(token, jwtSecret);
-
-  const user = await User.findOne({
-    _id: decoded.id,
-  });
-
-  if (!user) {
-    throw new Error();
+module.exports = async function (req, res, next) {
+  const user = new User(req.body);
+  //token provided by client
+  const tokenProvidedByUser =
+    req.header("x-authorization") || req.header("authorization");
+  //if token was sent
+  if (tokenProvidedByUser) {
+    try {
+      if (tokenProvidedByUser.startsWith("Bearer ")) {
+        // Remove Bearer from string
+        tokenProvidedByUser = tokenProvidedByUser.slice(
+          7,
+          tokenProvidedByUser.length
+        );
+      }
+      //decoding token
+      const decoded = jwt.verify(tokenProvidedByUser, jwtSecret);
+      if (decoded) {
+        /*
+         **valid token, giving client access to resource
+         */
+        req.token = tokenProvidedByUser;
+        req.user = user;
+        next();
+      }
+    } catch (err) {
+      res.status(400).json({
+        status: "Error",
+        message: "Access denied. Login",
+      });
+    }
+  } else {
+    return res.status(400).json({
+      status: "Error",
+      message: "Access denied. No toekn provided",
+    });
   }
-  req.token = token;
-  req.user = user;
-  next();
-
-  if (!token) {
-    res.send(response("Please authenticate!", error));
-
-  }
-}
-
-
-module.exports = auth;
+};
