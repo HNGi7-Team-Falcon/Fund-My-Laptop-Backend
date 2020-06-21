34420e05e7c1d4af39a11eb0ec84e8d14545c105
Chiagozie Jacqueline Robert-Maduekwe <cj4rober@uwaterloo.ca>
2020-06-21 12:20:44 -0400

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+const passport = require('passport');
 const router = require("express").Router();
 
 const userRoute = require("./userRoute");
@@ -9,6 +10,15 @@ const adminRequestRoute = require("./admin/requestRoute");
 
 module.exports = () => {
   router.get("/test", (req, res) => res.send("Yeah it works!"));
+  router.get('/login/twitter', passport.authenticate('twitter'));
+  router.get('/logout', (req, res, next) => { 
+    req.logout();  
+    res.redirect('/');
+  });
+  router.get('/return', passport.authenticate('twitter', { failureRedirect: '/' }),  
+    (req, res, next) => {res.redirect('/');
+
+  });
 
   router.use("/users", userRoute());
   router.use("/request", requestRoute());
