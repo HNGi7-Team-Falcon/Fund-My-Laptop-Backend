67cadb3a687023f944e12ed2018d6dd03fba7c01
onengiye richard <klevamane@gmail.com>
2020-06-21 17:13:53 +0100

    add todo notification for potential bug fix

--- src/services/RecommendationService.js
+++ src/services/RecommendationService.js
@@ -7,6 +7,9 @@ class RecommendationService {
   }
 
   async getUserRecommendations(id) {
+    //TODO check that the id passed is the id of a user
+    // that actually exists 
+    // update tests to reflect this change 
     const userRecommendations = await Recommendation.find({ requesterID: id })
     if (!userRecommendations.length) {
         throw new CustomError("No Recommendation was found for this user", 404);
