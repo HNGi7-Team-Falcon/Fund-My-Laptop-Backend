21b16495ca5cff471321a16c3751aa97457f74c2
Praise <opraise139@gmail.com>
2020-06-21 12:43:43 +0100

    Register Route:TEST

--- spec/registration.route.test.js
+++ spec/registration.route.test.js
@@ -1,75 +1,62 @@
 /**
- * @author _rotimi
-*/
+ * DON'T TOUCH THIS FILE - IT'S MY TASK
+ * @author @rotimi
+ * Story title: TEST: Registration Route
+ */
 
-const server = require('supertest');
-const app = require('../server');
-const dbHandler = require('./db-handler');
+const server = require("supertest");
+const app = require("../server");
+const dbHandler = require("./db-handler");
 
-const newRequestRoute = '/api/request/';
-const signUpRoute = '/api/users/';
-const User = require('../src/models/User');
+const signUpRoute = "/api/users/";
+const signInRoute = '/api/users/login';
+
 
 beforeAll(async () => {
   // spin database
   await dbHandler.connect();
 
   // Sign me up
-  await server(app).post(signUpRoute).send(me);
-
-//
+  const { body: { data: { token } } } = await server(app).post(signUpRoute).send(signUpDetails);
 });
 
 afterAll(async () => {
   // destroy database
   await dbHandler.closeDatabase();
-
 });
 
-describe('POST /api/users', () => {
-  it('should create a user && allow authenticated request', async() => {
-    const signupDetails = {
-        name: 'Jon-Bellion',
-        email: 'iRobot@gmail.com',
-        password: 'beautiful-mind'
-    }
-    const signUpResponse = await server(app).post(signUpRoute).send(signupDetails);
-    const { data: {token} } = signUpResponse.body; // get our authentication token
-    const res = await server(app).post(newRequestRoute).set('Bearer', token);
-
+describe('POST /api/user', () => {
+  it('should create a new user && login user', async () => {
+    const signInResponse = await server(app).post(signUpRoute).send(signUpDetails);
+    const { data: {token} } = signInResponse.body; // get our authentication token
+    const res = await server(app).post(signInRoute)
+      .send(signUpDetails)
+      .set('Authorization', 'Bearer ' + token);
+  
     expect(res.headers['x-auth']).not.toBeNull();
     expect(res.body._id).not.toBeNull();
     expect(res.body.email).not.toBeNull();
-    expect(res.statusCode).toEqual(200);
-
-    User.findOne({email}).then((user) => {
-        expect(user).not.toBeNull();
-        expect(user.password).not.toBe(password)
-        done();
-    });    
+    expect(res.statusCode).toBe(200);
   });
 
-  it('should reject a user with invalid email', async () => {
-    const signupDetails = {
-        name: 'Jon-Bellion',
-        email: 'iRobot',
-        password: 'beautiful-mind'
-    }
-    const signUpResponse = await server(app).post(signUpRoute).send(signupDetails);
-    const res = await server(app)
+  it('should reject a user with invalid mail', async () => {
+    const signUpDetails={email:'iRobot',password:"beautiful-mind"}
+    const res = await server(app).post(signUpRoute).send(signUpDetails);
+    expect(res.body).toEqual('Please input a valid mail account');
     expect(res.statusCode).toEqual(400);
   });
 
   it('should reject a user with invalid password', async () => {
-    const signupDetails = {
-        name: 'Jon-Bellion',
-        email: 'iRobot',
-        password: ''
-    }
-    const signUpResponse = await server(app).post(signUpRoute).send(signupDetails);
-    const res = await server(app)
+    const signUpDetails={email:'iRobot',password:""}
+    const res = await server(app).post(signUpRoute).send(signUpDetails);
+    expect(res.body).toEqual('Please input in a valid password');
     expect(res.statusCode).toEqual(400);
   });
-
 });
 
+const signUpDetails = {
+  name: 'Jon Bellion',
+  email: 'iRobot@gmail.com',
+  password: 'beautiful-mind',
+  verified: true,
+};
