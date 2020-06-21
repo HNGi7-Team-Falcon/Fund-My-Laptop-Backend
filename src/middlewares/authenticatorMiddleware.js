7fe56ca6dce0db9e7fd2bf0e25bb8e1f4b924d98
Ebuka Abraham <45390219+buka4rill@users.noreply.github.com>
2020-06-21 12:41:24 +0100

    Bug: Fixed bug in Auth Middleware

--- src/middlewares/authenticatorMiddleware.js
+++ src/middlewares/authenticatorMiddleware.js
@@ -1,45 +1,30 @@
 // const jwt = require("jsonwebtoken");
 // const User = require("../models/User");
+// const response = require("../utils/response");
 
 // const jwtSecret = process.env.JWT_SECRET;
 
-// module.exports = async function (req, res, next) {
-//   //token provided by client
-//   const tokenProvidedByUser =
-//     req.header("x-authorization") || req.header("authorization");
-//   //if token was sent
-//   if (tokenProvidedByUser) {
-//     try {
-//       if (tokenProvidedByUser.startsWith("Bearer ")) {
-//         // Remove Bearer from string
-//         tokenProvidedByUser = tokenProvidedByUser.slice(
-//           7,
-//           tokenProvidedByUser.length
-//         );
-//       }
-//       //decoding token
-//       const decoded = jwt.verify(tokenProvidedByUser, jwtSecret);
-//       if (decoded) {
-//         /*
-//          **valid token, giving client access to resource
-//          */
-//         req.body.token = tokenProvidedByUser;
-//         next();
-//       }
-//     } catch (err) {
-//       res.status(400).json({
-//         status: "Error",
-//         message: "Access denied. Login",
-//       });
-//     }
-//   } else {
-//     return res.status(400).json({
-//       status: "Error",
-//       message: "Access denied. No toekn provided",
-//     });
-//   }
-// };
+// const auth = async (req, res, next) => {
+//   const token = req.header("Authorization").replace("Bearer", "").trim();
 
+//   const decoded = jwt.verify(token, jwtSecret);
+
+//   const user = await User.findOne({
+//     _id: decoded.id,
+//   });
+
+//   if (!user) {
+//     throw new Error();
+//   }
+//   req.token = token;
+//   req.user = user;
+//   next();
+
+//   if (!token) {
+//     res.send(response("Please authenticate!", error));
+
+//   }
+// }
 
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
@@ -47,27 +32,47 @@ const response = require("../utils/response");
 
 const jwtSecret = process.env.JWT_SECRET;
 
-const auth = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer", "").trim();
+module.exports = async function (req, res, next) {
+  //token provided by client
+  const tokenProvidedByUser =
+    req.header("x-authorization") || req.header("authorization");
+  //if token was sent
+  if (tokenProvidedByUser) {
+    try {
+      if (tokenProvidedByUser.startsWith("Bearer ")) {
+        // Remove Bearer from string
+        tokenProvidedByUser = tokenProvidedByUser.slice(
+          7,
+          tokenProvidedByUser.length
+        );
+      }
+      //decoding token
+      const decoded = jwt.verify(tokenProvidedByUser, jwtSecret);
 
-  const decoded = jwt.verify(token, jwtSecret);
+      const user = await User.findOne({
+        _id: decoded.id,
+      });
+      
+      if (!user) {
+        throw new Error();
+      }
 
-  const user = await User.findOne({
-    _id: decoded.id,
-  });
+      req.user = user;
+      next();
 
-  if (!user) {
-    throw new Error();
+    } catch (err) {
+      res.status(400).json({
+        status: "Error",
+        message: "Access denied. Login",
+      });
+    }
+  } else {
+    return res.status(400).json({
+      status: "Error",
+      message: "Access denied. No toekn provided",
+    });
   }
-  req.token = token;
-  req.user = user;
-  next();
-
-  if (!token) {
-    res.send(response("Please authenticate!", error));
-
-  }
-}
+} 
 
 
 module.exports = auth;
