f5daf5fd1f20461770ef2c2466a121127689c09d
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-21 13:27:48 +0100

    fix/auth middleware

--- src/middlewares/authenticatorMiddleware.js
+++ src/middlewares/authenticatorMiddleware.js
@@ -49,7 +49,8 @@ require('../utils/env');
 const jwtSecret = process.env.JWT_SECRET;
 
 const auth = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer", "").trim();
+  const authHeader = req.header("Authorization");
+  const token = authHeader && authHeader.replace("Bearer", "").trim();
 
   const decoded = jwt.verify(token, jwtSecret);
 
