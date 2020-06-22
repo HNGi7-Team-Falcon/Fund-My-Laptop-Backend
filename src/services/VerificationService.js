01fb868f7fb4acb30a0283d3ea25e246545ab136
saucecodee <ashawinta@gmail.com>
2020-06-22 14:02:16 +0100

    feat: update verifcation

--- src/services/VerificationService.js
+++ src/services/VerificationService.js
@@ -4,22 +4,35 @@ const CustomError = require("../utils/CustomError");
 class VerifyService {
 
   async create(data) {
+    if (!data.videoUrl) throw new CustomError("Video Url is required")
+    if (!data.photoId) throw new CustomError("Photo ID is required")
+
+    if (!(await this.verifyBVN)) throw new CustomError("BVN verfication failed")
+
     const verify = new Verify(data);
     await verify.save();
-    return {
-      token: token,
-      uid: verify._id,
-      name: verify.name,
-      email: verify.email,
-    }
+
+    return null
   }
 
-  async update(data) {
-    
+  async update(verifyId, data) {
+    const verify = await Verify.findByIdAndUpdate(
+      { _id: verifyId },
+      data,
+      { new: true, }
+    );
 
-    return data
+    if (!verify) throw new CustomError("Verify dosen't exist", 404);
+
+    return verify;
   }
+
+  async verifyBVN() {
   
+
+    return true
+  }
+
 }
 
 module.exports = new VerifyService()
