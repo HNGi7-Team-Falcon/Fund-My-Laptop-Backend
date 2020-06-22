2b217e53d26289cc6f859545eaf543ce665ba9c4
Jezeh Priesten <jepriesten@gmail.com>
2020-06-22 17:02:48 +0100

    Added a comment to my code identifying it's author

--- src/services/RecommendationService.js
+++ src/services/RecommendationService.js
@@ -2,21 +2,23 @@ const CustomError = require("./../utils/CustomError");
 const Recommendation = require("../models/Recommendation");
 
 class RecommendationService {
+  /**
+   * @author Jezeh Priesten @TjeY
+   */
   async delete(id) {
     return Recommendation.findByIdAndRemove(id);
   }
 
   async getUserRecommendations(id) {
     //TODO check that the id passed is the id of a user
-    // that actually exists 
-    // update tests to reflect this change 
-    const userRecommendations = await Recommendation.find({ requesterID: id })
+    // that actually exists
+    // update tests to reflect this change
+    const userRecommendations = await Recommendation.find({ requesterID: id });
     if (!userRecommendations.length) {
-        throw new CustomError("No Recommendation was found for this user", 404);
+      throw new CustomError("No Recommendation was found for this user", 404);
     }
     return userRecommendations;
-}
-
+  }
 }
 
 module.exports = new RecommendationService();
