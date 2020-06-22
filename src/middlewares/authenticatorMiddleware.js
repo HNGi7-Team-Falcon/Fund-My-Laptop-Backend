4a0b6f14b90b495bf9ce71e112ab7bc12a893de5
HackinwaleDev <hackinwale.developer@gmail.com>
2020-06-22 03:04:59 +0100

    codebase cleanedup: bugs fixed

--- src/middlewares/authenticatorMiddleware.js
+++ src/middlewares/authenticatorMiddleware.js
@@ -48,42 +48,25 @@ require('../utils/env');
 
 const jwtSecret = process.env.JWT_SECRET;
 
-module.exports = async function (req, res, next) {
-  const user = new User(req.body);
-  //token provided by client
-  const tokenProvidedByUser =
-    req.header("x-authorization") || req.header("authorization");
-  //if token was sent
-  if (tokenProvidedByUser) {
-    try {
-      if (tokenProvidedByUser.startsWith("Bearer ")) {
-        // Remove Bearer from string
-        tokenProvidedByUser = tokenProvidedByUser.slice(
-          7,
-          tokenProvidedByUser.length
-        );
-      }
-      //decoding token
-      const decoded = jwt.verify(tokenProvidedByUser, jwtSecret);
-      if (decoded) {
-        /*
-         **valid token, giving client access to resource
-         */
-        req.token = tokenProvidedByUser;
-        req.user = user;
-        next();
-      }
-    } catch (err) {
-      res.status(400).json({
-        status: "Error",
-        message: "Access denied. Login",
-      });
-    }
-  } else {
-    return res.status(400).json({
-      status: "Error",
-      message: "Access denied. No toekn provided",
-    });
+const auth = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  const token = authHeader && authHeader.replace("Bearer", "").trim();
+
+  const decoded = jwt.verify(token, jwtSecret);
+
+  const user = await User.findOne({
+    _id: decoded.id,
+  });
+
+  if (!user) {
+    throw new Error();
+  }
+  req.token = token;
+  req.user = user;
+
+  if (!token) {
+    res.send(response("Please authenticate!", error));
+
   }
   next();
 }
